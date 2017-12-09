abf2f1df35748d0ae60565afc06aeb4613fe00e2
John Smith <John Smith>
2017-12-09 15:05:09 -0600

    price sorting script (unfinished)

---
+++ controllers/test.py
@@ -0,0 +1,15 @@
+import os
+
+transFile = open("../data sets/transactions/transaction log.txt", "r")
+priceList = []
+transName = transFile.readline()[:-1]
+for file in os.listdir("../data sets/prices"):
+	file = file[:-4]
+	if(file == transName):
+		print(transName)
+
+		priceFile = open("../data sets/prices/" + transName + ".txt")
+		for price in priceFile:
+			priceList.append(int(price))
+			priceList.sort()
+		print(priceList)
