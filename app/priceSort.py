feae3a2727ab708ec3f047969f406e08eb82737e
Timothy Yuen <theaznyune@gmail.com>
2017-12-10 01:18:45 -0500

    changed some things, not really sure which things

--- app/priceSort.py
+++ app/priceSort.py
@@ -17,27 +17,30 @@ for file in os.listdir("../data sets/transactions"):
     transactionName = row[1]
     # print("Read" + transactionName + " transaction!")
     
-    #find price data
-    for file in os.listdir("../data sets/prices"):
-      priceData = []
-      priceList = []
-      linkList = []
+    queryResults = pq.autoQueryProduct(transactionName)
+    print(queryResults)
 
-      fileName = file[:-4]
+    # #find price data
+    # for file in os.listdir("../data sets/prices"):
+    #   priceData = []
+    #   priceList = []
+    #   linkList = []
+
+    #   fileName = file[:-4]
       
-      #compare price file name with current transaction name
-      if(fileName == transactionName):
-        print(transactionName)
+    #   #compare price file name with current transaction name
+    #   if(fileName == transactionName):
+    #     print(transactionName)
 
-        #read price data
-        with open("../data sets/prices/" + transactionName + ".csv") as csvFile:
-          reader = csv.reader(csvFile, delimiter = ",")
-          for row in reader:
-            priceData.append(row)
+    #     #read price data
+    #     with open("../data sets/prices/" + transactionName + ".csv") as csvFile:
+    #       reader = csv.reader(csvFile, delimiter = ",")
+    #       for row in reader:
+    #         priceData.append(row)
 
         #using a Semantic3 API to grab product data
-        queryResults = pq.autoQueryProduct(transactionName)
-        print(queryResults)
+        # queryResults = pq.autoQueryProduct(transactionName)
+        # print(queryResults)
 
         #compare prices
         # for row in priceData:
--- app/productQuery.py
+++ app/productQuery.py
@@ -7,7 +7,6 @@ class ProductQuery:
       api_key = "SEM3188CBEA0C536322303E7C24718D47A3B",
       api_secret = "ZDI1NWU4NTQ3M2M4MjJiZDdlMWNlNDBmMTM0MTQ4OGU"
     )
-    self.results = []
 
   def queryProduct(self):
     userInputProduct = input("What product would you like to know about: ")
